73d9c35447d45d329209e54a3aaf53ff3c01ab96
spAnser <spenson66@gmail.com>
2025-10-16 16:05:58 -0500

    feat: Refactor inventory item emission for improved readability and change giveDoorKey to async

--- resources/inventory/src/server/server.ts
+++ resources/inventory/src/server/server.ts
@@ -6,15 +6,21 @@ RegisterCommand(
     const characterId = Number(args[0]);
     const itemId = GetHashKey(args[1]);
 
-    emitSocket('inventoryAddItem', `character:${characterId}`, itemId, Number(args[2] ?? 1), {}, (success: boolean) => {
-    });
+    emitSocket(
+      'inventoryAddItem',
+      `character:${characterId}`,
+      itemId,
+      Number(args[2] ?? 1),
+      {},
+      (success: boolean) => {},
+    );
   },
   false,
 );
 
 RegisterCommand(
   'giveDoorKey',
-  (source: number, args: string[], rawCommand: string) => {
+  async (source: number, args: string[], rawCommand: string) => {
     const characterId = Number(args[0]);
     const itemId = GetHashKey('PV_DOOR_KEY');
 
@@ -26,8 +32,7 @@ RegisterCommand(
       metadata.doorHash = Number(args[1]) << 0;
     }
 
-    emitSocket('inventoryAddItem', `character:${characterId}`, itemId, 1, metadata, (success: boolean) => {
-    });
+    emitSocket('inventoryAddItem', `character:${characterId}`, itemId, 1, metadata, (success: boolean) => {});
   },
   false,
 );

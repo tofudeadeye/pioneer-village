5b37cbf6cd644a2da4145687263d8690efbe333e
spAnser <spenson66@gmail.com>
2026-06-06 06:47:29 -0500

    feat(research): Police handcuff test

--- resources/[tools]/research/src/client/stuff/police.ts
+++ resources/[tools]/research/src/client/stuff/police.ts
@@ -1,5 +1,5 @@
-import { PVBase, PVGame, PVJobs, PVKeymapper, PVTarget } from '@lib/client';
-import { AnimFlag } from '@lib/flags';
+import { PVBase, PVGame, PVJobs, PVKeymapper, PVTarget, onResourceStop } from '@lib/client';
+import { AnimFlag, AttachPoint, PedConfigFlag } from '@lib/flags';
 import { Delay } from '@lib/functions';
 import { Vector3 } from '@lib/math';
 
@@ -86,3 +86,309 @@ PVTarget.AddTarget({
     sprite: { r: 255, g: 255, b: 255, a: 255 },
   },
 });
+
+PVTarget.AddTarget({
+  id: 'research::arrest_entity',
+  type: 'flag',
+  group: ['isEntity'],
+  data: [
+    {
+      id: 'arrest_entity',
+      label: 'Arrest',
+      icon: 'badge',
+      event: 'research:client:arrest_entity',
+      isEnabled(data) {
+        return (
+          (data.entity &&
+            DoesEntityExist(data.entity) &&
+            IsPedHuman(data.entity) &&
+            IsPedFacingPed(PlayerPedId(), data.entity, 45) &&
+            !Entity(data.entity).state.isHandcuffed) ||
+          false
+        );
+      },
+    },
+  ],
+  options: {
+    distance: 3.0,
+  },
+});
+
+PVTarget.AddTarget({
+  id: 'research::uncuff',
+  type: 'flag',
+  group: ['isEntity'],
+  data: [
+    {
+      id: 'uncuff',
+      label: 'Uncuff',
+      icon: 'badge',
+      event: 'research:client:uncuff',
+      isEnabled(data) {
+        return data.entity && DoesEntityExist(data.entity) && Entity(data.entity).state.isHandcuffed;
+      },
+    },
+  ],
+  options: {
+    distance: 3.0,
+  },
+});
+
+on('research:client:uncuff', async (entity: number) => {
+  const cuffRightNetId = Entity(entity).state.cuffsR;
+  const cuffLeftNetId = Entity(entity).state.cuffsL;
+  const ropeNetId = Entity(entity).state.rope;
+
+  const cuffsR = NetworkGetEntityFromNetworkId(cuffRightNetId);
+  const cuffsL = NetworkGetEntityFromNetworkId(cuffLeftNetId);
+  const rope = NetworkGetRopeIdFromNetworkId(ropeNetId);
+
+  PVBase.deleteEntity(cuffsR);
+  PVBase.deleteEntity(cuffsL);
+  DetachRopeFromEntity(rope, entity);
+  SetEnableHandcuffs(entity, false, false);
+  DeleteRope(rope);
+  Entity(entity).state.set('isHandcuffed', false, true);
+});
+
+on('research:client:arrest_entity', async (entity: number) => {
+  const coords = Vector3.fromArray(GetEntityCoords(entity, true));
+
+  const [rtn, z] = GetGroundZFor_3DCoord(coords.x, coords.y, coords.z, false);
+
+  if (rtn) {
+    coords.z = z;
+  } else {
+    coords.z -= 1;
+  }
+
+  const playerCoords = PVGame.playerCoords(true);
+  const direction = Vector3.fromArray(
+    GetOffsetFromEntityGivenWorldCoords(entity, playerCoords.x, playerCoords.y, playerCoords.z),
+  );
+
+  const [weapRtn, weaponHash] = GetCurrentPedWeapon(PlayerPedId(), false, AttachPoint.MainHand, false);
+
+  let animStyle: 'rifle' | 'pistol' = 'pistol';
+  if (weapRtn) {
+    if (IsWeaponTwoHanded(weaponHash)) {
+      animStyle = 'rifle';
+    }
+  }
+
+  let side: 'front' | 'back' | 'left' | 'right';
+  if (Math.abs(direction.x) > Math.abs(direction.y)) {
+    if (direction.x > 0) {
+      side = 'right';
+    } else {
+      side = 'left';
+    }
+  } else {
+    if (direction.y > 0) {
+      side = 'front';
+    } else {
+      side = 'back';
+    }
+  }
+
+  let playerHeading = GetEntityHeading(entity);
+
+  PVGame.taskPlayAnimAdvArray(
+    coords,
+    { x: 0, y: 0, z: playerHeading },
+    [
+      {
+        dict: `mech_melee@${animStyle}@_male@_ambient@_healthy@_streamed`,
+        anim: `arrest_enter_from_${side}_aiming_v1_att`,
+        flags: AnimFlag.OFFSET_POSITION,
+      },
+    ],
+    true,
+  );
+
+  PVGame.taskPlayAnimAdvArray(
+    coords,
+    { x: 0, y: 0, z: playerHeading },
+    [
+      {
+        entity,
+        dict: `mech_melee@${animStyle}@_male@_ambient@_healthy@_streamed`,
+        anim: `arrest_enter_from_${side}_aiming_v1_vic`,
+        flags: AnimFlag.OFFSET_POSITION,
+      },
+    ],
+    true,
+    entity,
+  );
+
+  await Delay(6_500);
+  SetEnableHandcuffs(entity, true, false);
+
+  const cuffsR = await PVGame.createObject('P_CS_SHACKLEWRIST02X');
+  PVGame.attachEntityToBoneName(
+    cuffsR,
+    'PH_R_HAND',
+    entity,
+    new Vector3(-0.075, 0, 0.01),
+    //
+    new Vector3(0, -65, 0),
+  );
+
+  const cuffsL = await PVGame.createObject('P_CS_SHACKLEWRIST02X');
+  PVGame.attachEntityToBoneName(
+    cuffsL,
+    'PH_L_HAND',
+    entity,
+    new Vector3(-0.075, 0, 0.01),
+    //
+    new Vector3(0, 65, 0),
+  );
+
+  // @ts-ignore
+  const rope = AddRope(
+    //
+    coords.x,
+    coords.y,
+    coords.z,
+    0,
+    0,
+    0,
+    0.001,
+    7,
+    0.005,
+    0.015,
+    0,
+    false,
+    false,
+    true,
+    1.0,
+    true,
+  );
+  console.log('rope', rope);
+  AttachEntitiesToRope(
+    // @ts-ignore
+    rope,
+    entity,
+    entity,
+    -0.11,
+    0,
+    0.05,
+    -0.11,
+    0,
+    -0.05,
+    0.1,
+    0,
+    0,
+    0, //'PH_L_HAND',
+    0, //'PH_R_HAND',
+    false,
+    37709,
+    7966,
+    0,
+    0,
+    true,
+    false,
+  );
+
+  const cuffRightNetId = NetworkGetNetworkIdFromEntity(cuffsR);
+  const cuffLeftNetId = NetworkGetNetworkIdFromEntity(cuffsL);
+  // @ts-ignore
+  const ropeNetId = NetworkGetNetworkIdFromRopeId(rope);
+
+  SetEntityCarryingFlag(entity, 2, true);
+  SetEntityCarryingFlag(entity, 6, true);
+  SetEntityCarryingFlag(entity, 5, true);
+  TaskStandStill(entity, -1);
+  SetPedKeepTask(entity, true);
+
+  Entity(entity).state.set('cuffsR', cuffRightNetId, true);
+  Entity(entity).state.set('cuffsL', cuffLeftNetId, true);
+  Entity(entity).state.set('rope', ropeNetId, true);
+  Entity(entity).state.set('isHandcuffed', true, true);
+});
+
+const tick = setTick(() => {
+  SetBlockingOfNonTemporaryEventsForAmbientPedsThisFrame(true);
+});
+
+onResourceStop(() => {
+  clearTick(tick);
+});
+
+/*
+
+mech_melee@rifle@_male@_ambient@_healthy@_streamed
+
+arrest_enter_from_back_aiming_v1_att
+arrest_enter_from_back_aiming_v1_vic
+arrest_enter_from_front_aiming_v1_att
+arrest_enter_from_front_aiming_v1_vic
+arrest_enter_from_right_aiming_v1_att
+arrest_enter_from_right_aiming_v1_vic
+arrest_enter_from_left_aiming_v1_att
+arrest_enter_from_left_aiming_v1_vic
+
+
+
+mech_melee@pistol@_male@_ambient@_healthy@_streamed
+
+arrest_enter_from_back_aiming_v1_att
+arrest_enter_from_back_aiming_v1_vic
+arrest_enter_from_front_aiming_v1_att
+arrest_enter_from_right_aiming_v1_vic
+arrest_enter_from_front_aiming_v1_vic
+arrest_enter_from_right_aiming_v1_att
+arrest_enter_from_left_aiming_v1_att
+arrest_enter_from_left_aiming_v1_vic
+
+*/
+
+RegisterCommand(
+  'anim_scene_test',
+  async (source: number, args: any[], rawCommand: string) => {
+    // Log({ source, args, rawCommand });
+
+    // PVGame.taskPlayAnimAdvArray(
+    //   coords,
+    //   { x: 0, y: 0, z: playerHeading },
+    //   [
+    //     {
+    //       dict: `mech_melee@${animStyle}@_male@_ambient@_healthy@_streamed`,
+    //       anim: `arrest_enter_from_${side}_aiming_v1_att`,
+    //       flags: AnimFlag.OFFSET_POSITION,
+    //     },
+    //   ],
+    //   true,
+    // );
+    //
+    // PVGame.taskPlayAnimAdvArray(
+    //   coords,
+    //   { x: 0, y: 0, z: playerHeading },
+    //   [
+    //     {
+    //       entity,
+    //       dict: `mech_melee@${animStyle}@_male@_ambient@_healthy@_streamed`,
+    //       anim: `arrest_enter_from_${side}_aiming_v1_vic`,
+    //       flags: AnimFlag.OFFSET_POSITION,
+    //     },
+    //   ],
+    //   true,
+    //   entity,
+    // );
+
+    const animStyle = 'pistol';
+    const animDict = `mech_melee@${animStyle}@_male@_ambient@_healthy@_streamed`;
+
+    await PVGame.loadAnimDict(animDict);
+
+    const animScene = CreateAnimScene(animDict, 2, 'arrest_enter_from_back_aiming_v1_att', false, true);
+
+    if (IsAnimSceneLoaded(animScene, true, false)) {
+      RequestAnimScenePlayList(animScene, 'arrest_enter_from_back_aiming_v1_att');
+      StartAnimScene(animScene);
+    }
+
+    DeleteAnimScene(animScene);
+  },
+  false,
+);
